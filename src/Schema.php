ea7f710baea3b9bc9ace50b3e780df5e14a544f1
goetas <goetas@978517f4-122b-427e-a458-7340bb581c75>
2012-09-27 16:07:27 +0000

    fix
    git-svn-id: http://svn.mercuriosistemi.com/goetas/xsd/trunk@669 978517f4-122b-427e-a458-7340bb581c75

--- src/Schema.php
+++ src/Schema.php
@@ -6,14 +6,14 @@ use DOMElement;
 use goetas\xml\XPath;
 class Schema {
 	const XSD_NS = 'http://www.w3.org/2001/XMLSchema';
-	
+
 	protected $elementQualification='unqualified';
 	protected $attributeQualification='unqualified';
-	
+
 	protected $elements;
 	protected $types;
 	protected $attributes;
-	
+
 	protected $ns;
 	/**
 	 * @var SchemaContainer
@@ -42,14 +42,14 @@ class Schema {
 	public function __construct(DOMElement $schema, SchemaContainer $container) {
 		$this->container = $container;
 		$this->schemaNode = $schema;
-		$this->ns = $this->schemaNode->getAttribute ( "targetNamespace" ); 
+		$this->ns = $this->schemaNode->getAttribute ( "targetNamespace" );
 		$this->elementQualification = $this->schemaNode->hasAttribute ( "elementFormDefault" )?$this->schemaNode->getAttribute ( "elementFormDefault" ):"unqualified";
 		$this->attributeQualification = $this->schemaNode->hasAttribute ( "attributeFormDefault" )?$this->schemaNode->getAttribute ( "attributeFormDefault" ):"unqualified";
-		
+
 		if($this->ns===self::XSD_NS){
 			$this->types["anySimpleType"] = new SimpleType($this, $schema);
-		} 
-	
+		}
+
 		$this->parseImports();
 	}
 	public function parseImports() {
@@ -59,44 +59,44 @@ class Schema {
 
 		foreach ($imports as $import){
 			$relPath = UrlUtils::resolve_url($this->schemaNode->ownerDocument->documentURI,$import->getAttribute("schemaLocation"));
-		
+
 			if($import->getAttribute("namespace")){
 				$this->container->addFinderFile($import->getAttribute("namespace"), $relPath);
 			}else{
 				$schema = new \DOMDocument('1.0','UTF-8');
 				$schema->load($relPath);
-				$this->schemas [] = new Schema($schema->documentElement, $this->container);	
-			}			
-		}	
+				$this->schemas [] = new Schema($schema->documentElement, $this->container);
+			}
+		}
 	}
 	public function createAnonymType(DOMElement $node) {
 		$xp = new XPath ( $node->ownerDocument );
 		$xp->registerNamespace ( "xsd", self::XSD_NS );
 		$nodi = $xp->query ( "ancestor::xsd:*[@name]", $node );
-		
+
 		$concat = array ($node->getAttribute ( "name" ) );
 		foreach ( $nodi as $nd ) {
 			$concat [] = $nd->getAttribute ( "name" );
 		}
-		$concat [] = "AnonymType";
-		
+		$concat [] = "__AnonymType";
+
 		$typeName = implode ( "_", $concat );
-		
+
 		$tnode = $xp->query("xsd:complexType|xsd:simpleType", $node)->item(0);
 		$tnode->setAttribute ( "name", $typeName );
-		
+
 		$this->types[$typeName] = $this->buildTypeFromNode($tnode);
-		
-		
-		
-		
+		$this->types[$typeName]->setAnonymous();
+
+
+
 		return $typeName;
 	}
 
 	private function buildTypeFromNode(DOMElement $node){
 		$xp = new XPath ( $node->ownerDocument );
 		$xp->registerNamespace ( "xsd", self::XSD_NS );
-		
+
 		switch ($node->localName) {
 			case "complexType" :
 				if ($xp->query ( "xsd:simpleContent", $node )->length) {
@@ -114,7 +114,7 @@ class Schema {
 	protected function loadType($name) {
 		$xp = new XPath ( $this->schemaNode->ownerDocument );
 		$xp->registerNamespace ( "xsd", self::XSD_NS );
-		
+
 		$nodes = $xp->query ( "xsd:complexType[@name='$name']|xsd:simpleType[@name='$name']", $this->schemaNode );
 		if ($nodes->length){
 			$node = $nodes->item ( 0 );
@@ -131,29 +131,29 @@ class Schema {
 	protected function loadElement($name) {
 		$xp = new XPath ( $this->schemaNode->ownerDocument );
 		$xp->registerNamespace ( "xsd", self::XSD_NS );
-		
+
 		$nodes = $xp->query ( "xsd:element[@name='$name']", $this->schemaNode );
 		if ($nodes->length){
 			$node = $nodes->item ( 0 );
-		
+
 			if (! $node->hasAttribute ( "type" )) {
-				
+
 				$typeName = $this->createAnonymType ( $node );
 
 				$prefix = $node->lookupPrefix ( $this->ns );
-				
-				$node->setAttribute ( "type", $prefix . ":" . $typeName );		
+
+				$node->setAttribute ( "type", $prefix . ":" . $typeName );
 
 			}
 			$elName = $node->getAttribute ( "name" );
 			$typeName = $node->getAttribute ( "type" );
-			
+
 			list ( $ns, $name, $prefix ) = self::findParts ( $node, $typeName );
-			
+
 			$type = $this->findType($ns, $name);
 
 			return new Element ( $this, $type, $node->getAttribute ( "name" ), 1, 1, false );
-			
+
 		}
 		foreach ($this->schemas as $schema){
 			try {
@@ -162,7 +162,7 @@ class Schema {
 			}
 		}
 		return false;
-	
+
 	}
 	/**
 	 *
@@ -191,7 +191,7 @@ class Schema {
 		return $this->ns;
 	}
 	/**
-	 * 
+	 *
 	 * @param string $name
 	 * @var Attribute
 	 */
@@ -199,7 +199,7 @@ class Schema {
 		return $this->attributes [$name];
 	}
 	/**
-	 * 
+	 *
 	 * @param string $name
 	 * @var Element
 	 */
@@ -216,13 +216,13 @@ class Schema {
 		if (! isset ( $this->types [$name] )) {
 			$this->types [$name] = $this->loadType ( $name );
 		}
-		if (! $this->types [$name] ) {			
+		if (! $this->types [$name] ) {
 			throw new \Exception ( "Non trovo il tipo $name in {$this->ns}" );
 		}
 		return $this->types [$name];
 	}
 	/**
-	 * 
+	 *
 	 * @param string $name
 	 * @var Type
 	 */
@@ -230,11 +230,11 @@ class Schema {
 		return $this->getSchema($ns)->getType ( $name );
 	}
 	/**
-	 * 
+	 *
 	 * @param string $name
 	 * @var Element
 	 */
 	public function findElement($ns, $name) {
 		return $this->getSchema($ns)->getElement ( $name );
 	}
-} 
+}
--- src/Type.php
+++ src/Type.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 namespace goetas\xml\xsd;
 
 abstract class Type {
@@ -7,16 +7,18 @@ abstract class Type {
 	 * @var goetas\xml\xsd\Schema
 	 */
 	protected $xsd;
-	
+
 	protected $name;
-	
+	protected $anonymous= false;
+
 	public function __construct(Schema $xsd, $name) {
 		$this->xsd = $xsd;
 		$this->name = $name;
 	}
+
 	/**
 	 * @return goetas\xml\xsd\Schema
-	 */	
+	 */
 	public function getSchema() {
 		return $this->xsd;
 	}
@@ -29,4 +31,12 @@ abstract class Type {
 	public function __toString() {
 		return "{".$this->getNs()."}". $this->getName();
 	}
-} 
+	public function getAnonymous() {
+		return $this->anonymous;
+	}
+
+	public function setAnonymous($anonymous = true) {
+		$this->anonymous = $anonymous;
+	}
+
+}
