6dde2b23a7ac355fab4ada5c66a361b450ed2bab
Asmir Mustafic <goetas@gmail.com>
2014-07-23 16:56:10 +0200

    Sami documentation

---
+++ sami.php
@@ -0,0 +1,18 @@
+<?php
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in('src')
+;
+
+
+return new Sami($iterator, array(
+    'theme'                => 'enhanced',
+    'title'                => 'Goetas XSD Reader',
+    'build_dir'            => __DIR__.'/apidoc',
+    'include_parent_data'  => true,
+    'default_opened_level' => 4,
+));
