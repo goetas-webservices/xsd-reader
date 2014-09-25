97e56589f19ae019d5bc985fbc0a81df856a0fcb
Ryan <rsul.dev@me.com>
2014-09-25 15:38:55 -0400

    Add more tests for URLs

--- tests/UrlUtilsTest.php
+++ tests/UrlUtilsTest.php
@@ -13,6 +13,24 @@ class UrlUtilsTest extends BaseTest {
         $this->assertEquals('http://example.com/test/test', UrlUtils::resolveRelativeUrl('http://example.com/', '/test/test'));
     }
 
+    public function testHttpPathsParent()
+    {
+        $this->assertEquals('http://example.com/test', UrlUtils::resolveRelativeUrl('http://example.com/parent/', '/test'));
+        $this->assertEquals('http://example.com/test', UrlUtils::resolveRelativeUrl('http://example.com/parent', '/test'));
+        $this->assertEquals('http://example.com/test', UrlUtils::resolveRelativeUrl('http://example.com//parent', '/test'));
+        $this->assertEquals('http://example.com/test/', UrlUtils::resolveRelativeUrl('http://example.com/parent', '/test/'));
+        $this->assertEquals('http://example.com/test/test', UrlUtils::resolveRelativeUrl('http://example.com/parent/', '/test/test'));
+    }
+
+    public function testHttpPathsParentRelative()
+    {
+        $this->assertEquals('http://example.com/test', UrlUtils::resolveRelativeUrl('http://example.com/parent/', '../test'));
+        $this->assertEquals('http://example.com/test/', UrlUtils::resolveRelativeUrl('http://example.com/parent/', '../test/'));
+        $this->assertEquals('http://example.com/test', UrlUtils::resolveRelativeUrl('http://example.com//parent/', '../test'));
+        $this->assertEquals('http://example.com/test/test', UrlUtils::resolveRelativeUrl('http://example.com/parent/', '../test/test'));
+    }
+
+
     public function testHttpAnchors()
     {
         $this->assertEquals('http://example.com/#test', UrlUtils::resolveRelativeUrl('http://example.com/', '#test'));
@@ -22,6 +40,7 @@ class UrlUtilsTest extends BaseTest {
         $this->assertEquals('http://example.com/test/test#test', UrlUtils::resolveRelativeUrl('http://example.com', 'test/test#test'));
     }
 
+
     public function testHttpQS()
     {
         $this->assertEquals('http://example.com/?test=1', UrlUtils::resolveRelativeUrl('http://example.com/', '?test=1'));
@@ -29,6 +48,28 @@ class UrlUtilsTest extends BaseTest {
         $this->assertEquals('http://example.com/test?test=1', UrlUtils::resolveRelativeUrl('http://example.com', 'test?test=1'));
         $this->assertEquals('http://example.com/test/test?test=1', UrlUtils::resolveRelativeUrl('http://example.com/', 'test/test?test=1'));
         $this->assertEquals('http://example.com/test/test?test=1', UrlUtils::resolveRelativeUrl('http://example.com', 'test/test?test=1'));
+    }
+
+
+
+    public function testFilePaths()
+    {
+        $this->assertEquals('file:///test', UrlUtils::resolveRelativeUrl('file:///', '/test'));
+        $this->assertEquals('file:///test', UrlUtils::resolveRelativeUrl('file:///', 'test'));
+    }
+
+
+
+    public function testRegularPaths()
+    {
+        $this->assertEquals('/test', UrlUtils::resolveRelativeUrl('/', '/test'));
+        $this->assertEquals('/test', UrlUtils::resolveRelativeUrl('/', 'test'));
+    }
+
+
+    public function testRegularPathsParent()
+    {
+        $this->assertEquals('/testing', UrlUtils::resolveRelativeUrl('/test/child', '../testing'));
     }
 
 } 
