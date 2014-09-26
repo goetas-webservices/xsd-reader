82e527c678237b4e684075f40ff5d92da4e3190b
Ryan <rsul.dev@me.com>
2014-09-26 09:16:17 -0400

    Handle unset indexes in parts array

--- src/Utils/UrlUtils.php
+++ src/Utils/UrlUtils.php
@@ -12,13 +12,13 @@ class UrlUtils
         );
 
         /* return if already absolute URL */
-        if (parse_url($rel, PHP_URL_SCHEME) != '' || substr($rel, 0, 2) == '//') {
+        if (parse_url($rel, PHP_URL_SCHEME) !== null || substr($rel, 0, 2) == '//') {
             return $rel;
         }
 
         /* queries and anchors */
         if ($rel[0] == '#' || $rel[0] == '?') {
-            return $base . $rel;
+            return $base.$rel;
         }
 
         /*
@@ -28,30 +28,37 @@ class UrlUtils
         $parts = parse_url($base);
 
         /* remove non-directory element from path */
-        $path = preg_replace('#/[^/]*$#', '', $parts["path"]);
+        $path = isset($parts['path']) ? preg_replace('#/[^/]*$#', '', $parts["path"]) : '';
 
         /* destroy path if relative url points to root */
-        if ($rel[0] == '/') {
+        if ($rel[0] === '/') {
             $path = '';
         }
 
-        /* dirty absolute URL */
-        $abs = $parts["host"].(isset($parts["port"])?(":".$parts["port"]):"").$path."/".$rel;
+        /* Build absolute URL */
+        $abs = '';
 
-        /* replace '//' or '/./' or '/foo/../' with '/' */
-
-        $n = 1;
-        do {
-            $abs = preg_replace($re, '/', $abs, - 1, $n);
-        } while ($n > 0);
-
-        /* absolute URL is ready! */
-        if($parts["scheme"] ){
-            return $parts["scheme"] . '://' . $abs;
-        }else{
-            return $abs;
+        if (isset($parts["host"])) {
+            $abs .= $parts['host'];
         }
 
+        if (isset($parts["port"])) {
+            $abs .= ":".$parts["port"];
+        }
+
+        $abs .= $path."/".$rel;
+
+        /* replace '//' or '/./' or '/foo/../' with '/' */
+        $n = 1;
+        do {
+            $abs = preg_replace($re, '/', $abs, -1, $n);
+        } while ($n > 0);
+
+        if (isset($parts["scheme"])) {
+            $abs = $parts["scheme"].'://'.$abs;
+        }
+
+        return $abs;
     }
 
 }
