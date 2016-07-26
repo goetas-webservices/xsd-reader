f33f441e9bc78e09873d0543ddcda01c399adcc8
Asmir Mustafic <goetas@gmail.com>
2016-07-26 10:42:17 +0200

    detect choiche nodes

--- src/SchemaReader.php
+++ src/SchemaReader.php
@@ -196,6 +196,14 @@ class SchemaReader
         if ($node->hasAttribute("minOccurs")) {
             $element->setMin((int)$node->getAttribute("minOccurs"));
         }
+
+        $xp = new \DOMXPath($node->ownerDocument);
+        $xp->registerNamespace('xs', 'http://www.w3.org/2001/XMLSchema');
+        
+        if ($xp->query('ancestor::xs:choice', $node)->length) {
+            $element->setMin(0);
+        }
+
         if ($node->hasAttribute("nillable")) {
             $element->setNil($node->getAttribute("nillable") == "true");
         }
