dd1e064ae83e5b283f2310123bec235982ffea38
SignpostMarv <me@signpostmarv.name>
2017-10-31 18:40:46 +0000

    rolling back the abstraction chain

--- src/SchemaReader.php
+++ src/SchemaReader.php
@@ -16,6 +16,7 @@ use GoetasWebservices\XML\XSDReader\Schema\Element\GroupRef;
 use GoetasWebservices\XML\XSDReader\Schema\Inheritance\Base;
 use GoetasWebservices\XML\XSDReader\Schema\Inheritance\Extension;
 use GoetasWebservices\XML\XSDReader\Schema\Inheritance\Restriction;
+use GoetasWebservices\XML\XSDReader\Schema\Item;
 use GoetasWebservices\XML\XSDReader\Schema\Schema;
 use GoetasWebservices\XML\XSDReader\Schema\Type\BaseComplexType;
 use GoetasWebservices\XML\XSDReader\Schema\Type\ComplexType;
@@ -23,7 +24,7 @@ use GoetasWebservices\XML\XSDReader\Schema\Type\ComplexTypeSimpleContent;
 use GoetasWebservices\XML\XSDReader\Schema\Type\SimpleType;
 use GoetasWebservices\XML\XSDReader\Schema\Type\Type;
 
-class SchemaReader extends SchemaReaderFillAbstraction
+class SchemaReader extends SchemaReaderFindAbstraction
 {
     /**
      * @param string $typeName
@@ -527,5 +528,49 @@ class SchemaReader extends SchemaReaderFillAbstraction
     protected function loadElementDef(Schema $schema, DOMElement $node)
     {
         return $this->loadAttributeOrElementDef($schema, $node, false);
+    }
+
+    /**
+     * @param bool $checkAbstract
+     */
+    protected function fillTypeNode(Type $type, DOMElement $node, $checkAbstract = false)
+    {
+        if ($checkAbstract) {
+            $type->setAbstract($node->getAttribute('abstract') === 'true' || $node->getAttribute('abstract') === '1');
+        }
+        static $methods = [
+            'restriction' => 'loadRestriction',
+            'extension' => 'maybeLoadExtensionFromBaseComplexType',
+            'simpleContent' => 'fillTypeNode',
+            'complexContent' => 'fillTypeNode',
+        ];
+
+        /**
+         * @var string[]
+         */
+        $methods = $methods;
+
+        $this->maybeCallMethodAgainstDOMNodeList($node, $type, $methods);
+    }
+
+    protected function fillItemNonLocalType(Item $element, DOMElement $node)
+    {
+        if ($node->getAttribute('type')) {
+            /**
+             * @var Type
+             */
+            $type = $this->findSomeType($element, $node, 'type');
+        } else {
+            /**
+             * @var Type
+             */
+            $type = $this->findSomeTypeFromAttribute(
+                $element,
+                $node,
+                ($node->lookupPrefix(self::XSD_NS).':anyType')
+            );
+        }
+
+        $element->setType($type);
     }
 }
