be3dd05415fb85b7e5f921c47d0215982231f85f
SignpostMarv <me@signpostmarv.name>
2017-10-31 23:23:18 +0000

    deduplicating code picked up by phpcpd

--- src/Schema/Element/ElementRef.php
+++ src/Schema/Element/ElementRef.php
@@ -6,32 +6,12 @@ use DOMElement;
 use GoetasWebservices\XML\XSDReader\Schema\Item;
 use GoetasWebservices\XML\XSDReader\SchemaReader;
 
-class ElementRef extends Item implements ElementSingle
+class ElementRef extends AbstractElementSingle
 {
     /**
      * @var ElementDef
      */
     protected $wrapped;
-
-    /**
-     * @var int
-     */
-    protected $min = 1;
-
-    /**
-     * @var int
-     */
-    protected $max = 1;
-
-    /**
-     * @var bool
-     */
-    protected $qualified = true;
-
-    /**
-     * @var bool
-     */
-    protected $nil = false;
 
     public function __construct(ElementDef $element)
     {
@@ -53,86 +33,6 @@ class ElementRef extends Item implements ElementSingle
     public function getType()
     {
         return $this->wrapped->getType();
-    }
-
-    /**
-     * @return int
-     */
-    public function getMin()
-    {
-        return $this->min;
-    }
-
-    /**
-     * @param int $min
-     *
-     * @return $this
-     */
-    public function setMin($min)
-    {
-        $this->min = $min;
-
-        return $this;
-    }
-
-    /**
-     * @return int
-     */
-    public function getMax()
-    {
-        return $this->max;
-    }
-
-    /**
-     * @param int $max
-     *
-     * @return $this
-     */
-    public function setMax($max)
-    {
-        $this->max = $max;
-
-        return $this;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isQualified()
-    {
-        return $this->qualified;
-    }
-
-    /**
-     * @param bool $qualified
-     *
-     * @return $this
-     */
-    public function setQualified($qualified)
-    {
-        $this->qualified = is_bool($qualified) ? $qualified : (bool) $qualified;
-
-        return $this;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isNil()
-    {
-        return $this->nil;
-    }
-
-    /**
-     * @param bool $nil
-     *
-     * @return $this
-     */
-    public function setNil($nil)
-    {
-        $this->nil = is_bool($nil) ? $nil : (bool) $nil;
-
-        return $this;
     }
 
     /**
