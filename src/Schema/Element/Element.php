8e0888101bee26899590fb10492ff1fa11ce4507
SignpostMarv <me@signpostmarv.name>
2017-11-01 21:35:30 +0000

    rolling back abstraction chain

--- src/Schema/Element/Element.php
+++ src/Schema/Element/Element.php
@@ -8,36 +8,4 @@ use GoetasWebservices\XML\XSDReader\SchemaReader;
 
 class Element extends AbstractElementSingle implements ElementItem
 {
-    /**
-     * @return Element
-     */
-    public static function loadElement(
-        SchemaReader $reader,
-        Schema $schema,
-        DOMElement $node
-    ) {
-        $element = new self($schema, $node->getAttribute('name'));
-        $element->setDoc(SchemaReader::getDocumentation($node));
-
-        $reader->fillItem($element, $node);
-
-        SchemaReader::maybeSetMax($element, $node);
-        SchemaReader::maybeSetMin($element, $node);
-
-        $xp = new \DOMXPath($node->ownerDocument);
-        $xp->registerNamespace('xs', 'http://www.w3.org/2001/XMLSchema');
-
-        if ($xp->query('ancestor::xs:choice', $node)->length) {
-            $element->setMin(0);
-        }
-
-        if ($node->hasAttribute('nillable')) {
-            $element->setNil($node->getAttribute('nillable') == 'true');
-        }
-        if ($node->hasAttribute('form')) {
-            $element->setQualified($node->getAttribute('form') == 'qualified');
-        }
-
-        return $element;
-    }
 }
--- src/SchemaReader.php
+++ src/SchemaReader.php
@@ -207,8 +207,7 @@ class SchemaReader
                 $childNode
             );
         } else {
-            $element = Element::loadElement(
-                $this,
+            $element = $this->loadElement(
                 $elementContainer->getSchema(),
                 $childNode
             );
@@ -1648,6 +1647,38 @@ class SchemaReader
                 $callback();
             }
         };
+    }
+
+    /**
+     * @return Element
+     */
+    public function loadElement(
+        Schema $schema,
+        DOMElement $node
+    ) {
+        $element = new Element($schema, $node->getAttribute('name'));
+        $element->setDoc(SchemaReader::getDocumentation($node));
+
+        $this->fillItem($element, $node);
+
+        SchemaReader::maybeSetMax($element, $node);
+        SchemaReader::maybeSetMin($element, $node);
+
+        $xp = new \DOMXPath($node->ownerDocument);
+        $xp->registerNamespace('xs', 'http://www.w3.org/2001/XMLSchema');
+
+        if ($xp->query('ancestor::xs:choice', $node)->length) {
+            $element->setMin(0);
+        }
+
+        if ($node->hasAttribute('nillable')) {
+            $element->setNil($node->getAttribute('nillable') == 'true');
+        }
+        if ($node->hasAttribute('form')) {
+            $element->setQualified($node->getAttribute('form') == 'qualified');
+        }
+
+        return $element;
     }
 
     /**
