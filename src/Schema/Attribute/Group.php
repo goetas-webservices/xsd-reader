4ad5508b9b20585d7f93fe34fe7a32e65152ee5a
SignpostMarv <me@signpostmarv.name>
2017-11-02 20:26:25 +0000

    php-cs-fixer

--- src/Schema/Attribute/Group.php
+++ src/Schema/Attribute/Group.php
@@ -3,7 +3,6 @@
 namespace GoetasWebservices\XML\XSDReader\Schema\Attribute;
 
 use GoetasWebservices\XML\XSDReader\Schema\AbstractNamedGroupItem;
-use GoetasWebservices\XML\XSDReader\Schema\Schema;
 
 class Group extends AbstractNamedGroupItem implements AttributeItem, AttributeContainer
 {
--- src/Schema/Element/Group.php
+++ src/Schema/Element/Group.php
@@ -3,7 +3,6 @@
 namespace GoetasWebservices\XML\XSDReader\Schema\Element;
 
 use GoetasWebservices\XML\XSDReader\Schema\AbstractNamedGroupItem;
-use GoetasWebservices\XML\XSDReader\Schema\Schema;
 
 class Group extends AbstractNamedGroupItem implements ElementItem, ElementContainer
 {
