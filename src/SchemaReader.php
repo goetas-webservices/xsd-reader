b5836d9afc78dbaa6815d3b5e6aa5cace8ecc49f
Asmir Mustafic <goetas@gmail.com>
2018-02-09 15:41:06 +0100

    code stlye

--- src/SchemaReader.php
+++ src/SchemaReader.php
@@ -1176,9 +1176,6 @@ class SchemaReader
      */
     protected $globalSchema;
 
-    /**
-     * @return Schema
-     */
     public function getGlobalSchema(): Schema
     {
         if (!($this->globalSchema instanceof Schema)) {
@@ -1220,20 +1217,15 @@ class SchemaReader
             }
         }
 
-        /**
-         * @var Schema
-         */
-        $out = $this->globalSchema;
-
-        if (!($out instanceof Schema)) {
+        if (!($this->globalSchema instanceof Schema)) {
             throw new TypeException('Globa schema not discoverd');
         }
 
-        return $out;
+        return $this->globalSchema;
     }
 
     /**
-     * @param DOMElement[] $nodes
+     * @param DOMNode[] $nodes
      */
     public function readNodes(array $nodes, string $file = null): Schema
     {
