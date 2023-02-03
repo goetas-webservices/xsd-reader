4c5d005ee4fc4e2f16c0dfa98d59acabe81e6ff5
Axel Guckelsberger <info@guite.de>
2023-02-03 11:45:28 +0100

    partial revert

--- src/Schema/Attribute/AttributeDef.php
+++ src/Schema/Attribute/AttributeDef.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace GoetasWebservices\XML\XSDReader\Schema\Attribute;
 
 /**
- * An AttributeDef represents a root concept of a schema.
+ * An AttributeDef represents an attribute definition in the root context of a schema.
  * It can be referenced by an AttributeRef.
  */
 class AttributeDef extends AbstractAttributeItem
--- src/Schema/Element/ElementDef.php
+++ src/Schema/Element/ElementDef.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace GoetasWebservices\XML\XSDReader\Schema\Element;
 
 /**
- * An ElementDef represents a root concept of a schema.
+ * An ElementDef represents an element definition in the root context of a schema.
  * It can be referenced by an ElementRef.
  */
 class ElementDef extends AbstractElementSingle implements ElementItem
